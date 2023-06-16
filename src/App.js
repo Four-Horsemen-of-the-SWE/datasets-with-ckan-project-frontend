b24785888583ff71ce1d39aee4e4bc349f6c80af
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2023-06-16 23:37:55 +0700

    feat: init router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 // import components
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
