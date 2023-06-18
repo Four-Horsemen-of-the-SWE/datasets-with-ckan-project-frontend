ca271a0560920edf16828af34bd9d45e46d71ac5
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2023-06-18 20:00:41 +0700

    feat: add detail and upload form in create datasets modal

--- src/components/Datasets/CreateDatasetsModal.js
+++ src/components/Datasets/CreateDatasetsModal.js
@@ -1,14 +1,60 @@
-import { Button, message, Steps, theme, Modal } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+import {
+  Button,
+  message,
+  Steps,
+  theme,
+  Modal,
+  Form,
+  Input,
+  Upload,
+} from "antd";
 import { useState } from "react";
+
+const { TextArea } = Input;
+const { Dragger } = Upload;
+
+const DetailForm = () => {
+  return (
+    <Form layout="vertical" className="p-4">
+      <Form.Item label="Datasets Name" name="dataset_name">
+        <Input size="large" placeholder="dataset name" />
+      </Form.Item>
+      <Form.Item label="Source" name="source">
+        <Input size="large" placeholder="reference url" />
+      </Form.Item>
+      <Form.Item label="Notes" name="notes">
+        <TextArea rows={4} placeholder="description" />
+      </Form.Item>
+    </Form>
+  );
+};
+
+const UploadForm = () => {
+  return (
+    <Dragger multiple>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">
+        Click or drag file to this area to upload
+      </p>
+      <p className="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibited from uploading
+        company data or other banned files.
+      </p>
+    </Dragger>
+  );
+};
 
 const steps = [
   {
     title: "Fill Details",
-    content: "First-content",
+    content: <DetailForm />,
   },
   {
     title: "Upload Resource File",
-    content: "Second-content",
+    content: <UploadForm />,
   },
 ];
 
