9ce80a03bd775be7d4a8ab1adc30d9a37abcedc7
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2023-06-19 03:00:07 +0700

    feat: add drawer

---
+++ src/components/DrawerView.js
@@ -0,0 +1,71 @@
+import {
+  UserOutlined,
+  DatabaseOutlined,
+  BookOutlined,
+  EditOutlined,
+} from "@ant-design/icons";
+import { Button, Drawer, List, Space, Popconfirm, Typography } from "antd"
+
+const { Link } = Typography
+
+const data = [
+  {
+    icon: <UserOutlined />,
+    label: (
+      <Link href="/profile/me" style={{ color: "#000" }}>
+        Your Profile
+      </Link>
+    ),
+  },
+  {
+    icon: <DatabaseOutlined />,
+    label: "Your Datasets",
+  },
+  {
+    icon: <BookOutlined />,
+    label: "Your Bookmarked",
+  },
+  {
+    icon: <EditOutlined />,
+    label: "Update Profile",
+  },
+  {
+    icon: (
+      <Popconfirm
+        title="Logout ?"
+        description="Are you sure to logout ?"
+        placement="right"
+        onConfirm={() => alert("Logout")}
+        okText="Yes"
+        cancelText="No"
+      >
+        <Button type="primary" danger>
+          Logout
+        </Button>
+      </Popconfirm>
+    ),
+  },
+];
+
+
+export default function DrawerView({ isDrawerOpen, close }) {
+  return (
+    <Drawer
+      title="Profile"
+      placement="right"
+      open={isDrawerOpen}
+      onClose={close}
+    >
+      <List
+        size="middle"
+        dataSource={data}
+        renderItem={(item) => <List.Item>
+          <Space size={16}>
+            {item.icon}
+            {item.label}
+          </Space>
+        </List.Item>}
+      />
+    </Drawer>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
-import { UserOutlined } from "@ant-design/icons";
-import { Avatar, Button, Card, Popconfirm, Input, Typography } from "antd";
+import { UserOutlined, PlusOutlined, MessageOutlined } from "@ant-design/icons";
+import { Button, Card, Input, Typography, Space } from "antd";
 import Logo from "../images/folders.svg";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
+import DrawerView from "./DrawerView";
 
 const { Link } = Typography;
 const { Search } = Input;
 
 export default function Navbar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   const [isLogin, setIsLogin] = useState(true)
   const location = useLocation();
 
@@ -20,59 +22,61 @@ export default function Navbar() {
     return null;
   }
   return (
-    <Card bordered size="small" className="w-full shadow-md rounded-lg">
-      <div className="container mx-auto flex flex-row justify-between items-center">
-        <div className="flex items-center">
-          <Link href="/">
-            <img
-              src={Logo}
-              alt="logo"
-              width={50}
-              height={50}
-              className="m-0 p-0 hidden sm:block"
-            />
-          </Link>
-          <Link
-            href="/datasets"
-            className="ml-2 sm:ml-4"
-            style={{ color: "#000", textTransform: "uppercase" }}
-          >
-            Datasets
-          </Link>
-        </div>
-        <div className="flex items-center sm:mt-0">
-          <div className="flex sm:mt-0 ml-2 sm:ml-4 items-center">
-            {isLogin ? (
-              <>
-                <Link href="/profile/me">
-                  <Button className="mr-2 sm:mr-4">Profile</Button>
-                </Link>
-                <Popconfirm
-                  title="Logout ?"
-                  description="Are you sure to logout ?"
-                  placement="bottom"
-                  onConfirm={() => alert("Logout")}
-                  okText="Yes"
-                  cancelText="No"
-                >
-                  <Button type="primary" danger>
-                    Logout
-                  </Button>
-                </Popconfirm>
-              </>
-            ) : (
-              <>
-                <Link href="/login">
-                  <Button className="mr-2 sm:mr-4">Login</Button>
-                </Link>
-                <Link href="/register">
-                  <Button type="primary">Logout</Button>
-                </Link>
-              </>
-            )}
+    <>
+      <DrawerView
+        isDrawerOpen={isDrawerOpen}
+        close={() => setIsDrawerOpen(false)}
+      />
+
+      <Card bordered size="small" className="w-full shadow-md rounded-lg">
+        <div className="container mx-auto flex flex-row justify-between items-center">
+          <div className="flex items-center">
+            <Link href="/">
+              <img
+                src={Logo}
+                alt="logo"
+                width={50}
+                height={50}
+                className="m-0 p-0 hidden sm:block"
+              />
+            </Link>
+            <Link
+              href="/datasets"
+              className="ml-2 sm:ml-4"
+              style={{ color: "#000", textTransform: "uppercase" }}
+            >
+              Datasets
+            </Link>
+          </div>
+          <div className="flex items-center sm:mt-0">
+            <div className="flex sm:mt-0 ml-2 sm:ml-4 items-center">
+              {isLogin ? (
+                <>
+                  <Space>
+                    <Button icon={<PlusOutlined />}>Create Datasets</Button>
+                    <Button icon={<MessageOutlined />} />
+                    <Button
+                      icon={<UserOutlined />}
+                      onClick={() => setIsDrawerOpen(!isDrawerOpen)}
+                    >
+                      Profile
+                    </Button>
+                  </Space>
+                </>
+              ) : (
+                <>
+                  <Link href="/login">
+                    <Button className="mr-2 sm:mr-4">Login</Button>
+                  </Link>
+                  <Link href="/register">
+                    <Button type="primary">Logout</Button>
+                  </Link>
+                </>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 }
