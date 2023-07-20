ccd8e1c1e82e097a8ac0bae5c1d22c45318d9e43
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2023-07-21 01:37:18 +0700

    feat: add new filter card in AllDatasets.js

--- src/pages/Datasets/AllDatasets.js
+++ src/pages/Datasets/AllDatasets.js
@@ -1,5 +1,5 @@
 import { SearchOutlined } from '@ant-design/icons';
-import { Col, Input, Row, Space, Typography, Select, Divider, Empty, DatePicker, Button, Checkbox, Collapse } from "antd";
+import { Col, Input, Row, Space, Typography, Select, Divider, Empty, DatePicker, Button, Checkbox, Collapse, Card, List, Tag, AutoComplete } from "antd";
 import axios from "axios";
 
 // import components
@@ -7,6 +7,59 @@ import DatasetsCard from "../../components/Card/DatasetsCard";
 import { useEffect, useState } from 'react';
 
 const { Title, Text } = Typography;
+
+const tag_data = [
+  "Technology",
+  "Programming",
+  "Web Development",
+  "Software",
+  "Data Science",
+  "Machine Learning",
+  "Artificial Intelligence",
+  "Cybersecurity",
+  "Cloud Computing",
+  "Mobile App Development",
+  "IoT (Internet of Things)",
+  "Big Data",
+  "Blockchain",
+  "DevOps",
+  "Frontend Development",
+  "Backend Development",
+];
+
+const license_data = [
+  "MIT License",
+  "Apache License 2.0",
+  "GNU General Public License (GPL)",
+  'BSD 3-Clause "New" or "Revised" License',
+  "Mozilla Public License (MPL)",
+  "GNU Lesser General Public License (LGPL)",
+  "Eclipse Public License (EPL)",
+  "Creative Commons Licenses",
+  "ISC License",
+  "Boost Software License",
+  "GNU Affero General Public License (AGPL)",
+  "GNU General Public License (GPL) v3",
+  "Common Development and Distribution License (CDDL)",
+  "Artistic License 2.0",
+  "WTFPL – Do What the F*ck You Want to Public License",
+  "Unlicense",
+  "Microsoft Public License (MS-PL)",
+  "Eclipse Distribution License (EDL)",
+  "Open Software License (OSL)",
+  "Dojo Foundation License",
+  "Perl License",
+  "Python Software Foundation License (PSF)",
+  "SIL Open Font License (OFL)",
+  "European Union Public License (EUPL)",
+  "Apple Public Source License (APSL)",
+  "Zend Framework License",
+  "Facebook BSD + Patents License",
+  "Amazon Software License",
+  "Google Open Source Project License",
+  "Netflix Software License",
+  "MongoDB Server Side Public License (SSPL)",
+];
 
 export default function AllDatasets() {
   const [selectedTags, setSelectedTags] = useState([]);
@@ -97,27 +150,104 @@ export default function AllDatasets() {
 
       <div className="container mx-auto">
         <Row gutter={[18, 18]}>
-          {allDatasets.length ? (
-            allDatasets.map((item, key) => (
-              <Col xs={12} md={12} lg={6} key={key}>
-                <DatasetsCard
-                  id={item.id}
-                  thumbnail={item?.thumbnail}
-                  title={item.title}
-                  notes={item.notes}
-                  metadata_modified={item.metadata_modified}
-                  author={item.author}
+          {/* show filter options, such asssssss date, tag, license */}
+          <Col xs={24} lg={6}>
+            <Card>
+              {/* Tag Section */}
+              <div className="mb-10">
+                <Title level={5} style={{ marginTop: 0 }}>
+                  Tags
+                </Title>
+                <AutoComplete
+                  placeholder="Search tags here."
+                  className="w-full mb-2"
                 />
-              </Col>
-            ))
-          ) : (
-            <Col
-              span={24}
-              className="w-full h-96 flex items-center justify-center"
-            >
-              <Empty />
-            </Col>
-          )}
+                <div className="overflow-y-auto overflow-x-hidden max-h-56">
+                  {tag_data.map((item) => (
+                    <div
+                      style={{
+                        backgroundColor: "#F7F9FC",
+                        padding: "4px 10px",
+                        marginBottom: "5px",
+                        borderRadius: "7px",
+                        cursor: "pointer",
+                      }}
+                    >
+                      {item}
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              {/* License Section */}
+              <div className="mb-10">
+                <Title level={5} style={{ marginTop: 0 }}>
+                  License
+                </Title>
+                <AutoComplete
+                  placeholder="Search license here."
+                  className="w-full mb-2"
+                />
+                <div className="overflow-y-auto overflow-x-hidden max-h-56">
+                  {license_data.map((item) => (
+                    <div
+                      style={{
+                        backgroundColor: "#F7F9FC",
+                        padding: "4px 10px",
+                        marginBottom: "5px",
+                        borderRadius: "7px",
+                        cursor: "pointer",
+                      }}
+                    >
+                      {item}
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              {/* Date section */}
+              <div className="mb-10">
+                <Title level={5} style={{ marginTop: 0 }}>
+                  Date
+                </Title>
+                <DatePicker.RangePicker
+                  placement="bottomRight"
+                  className="w-full"
+                />
+              </div>
+
+              {/* Clear button */}
+              <Button block={true} danger={true}>
+                Clear
+              </Button>
+            </Card>
+          </Col>
+          {/* show all datasets in database (ckan) */}
+          <Col xs={24} lg={18}>
+            <Row gutter={[18, 18]}>
+              {allDatasets.length ? (
+                allDatasets.map((item, key) => (
+                  <Col xs={12} md={12} lg={6} key={key}>
+                    <DatasetsCard
+                      id={item.id}
+                      thumbnail={item?.thumbnail}
+                      title={item.title}
+                      notes={item.notes}
+                      metadata_modified={item.metadata_modified}
+                      author={item.author}
+                    />
+                  </Col>
+                ))
+              ) : (
+                <Col
+                  span={24}
+                  className="w-full h-96 flex items-center justify-center"
+                >
+                  <Empty />
+                </Col>
+              )}
+            </Row>
+          </Col>
         </Row>
       </div>
     </>
