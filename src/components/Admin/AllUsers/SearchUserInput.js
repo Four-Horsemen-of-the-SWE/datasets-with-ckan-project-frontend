66bab5eff0123b932e051cf9ea97fd8652b41660
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2023-08-24 23:24:00 +0700

    feat: add autocomplete in user page

---
+++ src/components/Admin/AllUsers/SearchUserInput.js
@@ -0,0 +1,36 @@
+import { AutoComplete, Button, Typography } from 'antd'
+import axios from 'axios';
+import React, { useState } from 'react'
+
+export default function SearchUserInput() {
+  const [users, setUsers] = useState([]);
+
+  const handleSearch = async(text) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_CKAN_API_ENDPOINT}/users/auto_complete?q=${text}`
+      );
+      if(response.data.ok) {
+        setUsers(response.data.result?.map(item => ({value: item.name, label: item.fullname})));
+      }
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+      <Typography.Text strong={true}>Search user</Typography.Text>
+      <AutoComplete
+        options={users}
+        size="large"
+        className="w-full mb-5"
+        placeholder="search user from name"
+        allowClear={true}
+        onSearch={handleSearch}
+        onClear={() => setUsers([])}
+        onSelect={(value) => window.location.href = `/profile/${value}`}
+      />
+    </>
+  );
+}
--- src/pages/Admin/Page/AllUsersPage.js
+++ src/pages/Admin/Page/AllUsersPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import AllUsersTable from '../../../components/Admin/AllUsers/AllUsersTable'
+import SearchUserInput from '../../../components/Admin/AllUsers/SearchUserInput'
 
 export default function AllUsersPage() {
   return (
     <>
+      <SearchUserInput />
+
       <AllUsersTable />
     </>
   )
