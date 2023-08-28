17f70a62341d144cdd6aa34ee5dd30739c090e71
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2023-08-28 23:53:06 +0700

    add csv table

--- src/components/Datasets/VisualizationModal.js
+++ src/components/Datasets/VisualizationModal.js
@@ -1,22 +1,25 @@
 import { CloudDownloadOutlined } from "@ant-design/icons";
 import { Modal, Image, Button } from 'antd'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { getVisualize } from "../../lib/getVisualization";
+
 
 export default function VisualizationModal({ resource_id, mimetype, format, url, open, close }) {
   const content = getVisualize(mimetype, url);
+
   return (
     <Modal
       title="Visualization window"
       open={open}
       onCancel={close}
+      maskClosable={false}
       footer={[
         <Button onClick={close}>Cancel</Button>,
         <Button icon={<CloudDownloadOutlined />}>Download</Button>,
         <Button type="primary">Ok</Button>,
       ]}
     >
-      {content()}
+      {content}
     </Modal>
   );
 }
--- src/lib/getVisualization.js
+++ src/lib/getVisualization.js
@@ -1,4 +1,8 @@
-import { Image } from "antd"
+import React, { useState, useEffect } from "react";
+import { AutoSizer, Table, Column } from "react-virtualized";
+import "react-virtualized/styles.css"; // Import the styles
+import { Image } from "antd";
+import Papa from "papaparse";
 import mime from "mime";
 
 const imageMimeTypes = [
@@ -14,22 +18,92 @@ const imageMimeTypes = [
 ];
 
 export function getVisualize(mimetype, url) {
-  const content = () => {
-    if (imageMimeTypes.includes(mimetype)) {
-      return visualizeImage(url);
-    } else {
-      return null;
-    }
-  };
-
-  return content;
+  if (imageMimeTypes.includes(mimetype)) {
+    return <VisualizeImage url={url} />;
+  } else if (mimetype === "text/csv") {
+    return (
+      <VisualizeCSV
+        csvFilePath={
+          "http://127.0.0.1:5000/dataset/fab1f121-733c-4001-bfc7-5d62c3b2d0ef/resource/5b6fbb81-defe-4c7b-8b09-cf3183398b09/download/1e1e_netflix-userbase.csv"
+        }
+      />
+    );
+  } else {
+    return null;
+  }
 }
 
-export function visualizeImage(url) {
+export function VisualizeImage({url}) {
   return (
     <Image
-      src={"asd"}
+      src={url}
       fallback={process.env.PUBLIC_URL + "/images/placeholder/image.jpg"}
     />
   );
 }
+
+export function VisualizeCSV({ csvFilePath }) {
+  const [csvData, setCsvData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await new Promise((resolve, reject) => {
+          Papa.parse(csvFilePath, {
+            download: true,
+            header: true,
+            skipEmptyLines: true,
+            complete: (result) => {
+              resolve(result.data);
+            },
+            error: (error) => {
+              reject(error);
+            },
+          });
+        });
+
+        setCsvData(result);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [csvFilePath]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(csvData)
+
+  return (
+    <div style={{ minHeight: "60vh", width: "auto", overflow: "auto", marginTop: "2em" }}>
+      {csvData?.length !== 0 && (
+        <AutoSizer>
+          {({ width, height }) => (
+            <Table
+              width={width}
+              height={height}
+              headerHeight={20}
+              rowHeight={30}
+              rowCount={csvData.length}
+              rowGetter={({ index }) => csvData[index]}
+            >
+              {Object.keys(csvData[0]).map((key, index) => (
+                <Column key={index} label={key} dataKey={key} width={150} />
+              ))}
+            </Table>
+          )}
+        </AutoSizer>
+      )}
+    </div>
+  );
+}
+
+/**
+
+*/
